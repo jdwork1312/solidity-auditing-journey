197bdde46c7e521959c24ed735acd60b9c9a5a4a
jdwork1312 <jdwork1312@gmail.com>
2025-03-21 11:26:11 +0200

    Create Access-Control.sol

---
+++ AUDITING/Access-Control.sol
@@ -0,0 +1,2 @@
+// What it is: Functions that should be restricted to certain users (e.g., owner) are exposed to everyone, allowing unauthorized users to call sensitive functions.
+How to fix: Use modifiers (e.g., onlyOwner) to restrict access to sensitive functions.
