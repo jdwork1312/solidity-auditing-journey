2417545e3d425005f1af168e02f42cfd8b0abd0c
jdwork1312 <jdwork1312@gmail.com>
2025-03-21 11:28:31 +0200

    Create Timestamp-Dependency.sol

---
+++ AUDITING/Timestamp-Dependency.sol
@@ -0,0 +1,2 @@
+//What it is: Using block timestamps for important logic (e.g., time-based conditions), which can be manipulated by miners within a small range.
+How to fix: Use time buffers or block numbers to mitigate the impact of miner manipulation.
