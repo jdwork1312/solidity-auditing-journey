f9016cd93e21c123412755d08202003135df1ec9
jdwork1312 <jdwork1312@gmail.com>
2025-03-24 17:11:31 +0200

    Update Vaunrable-Oracle-manipulation.sol

--- AUDITING/Oracle-Manipulation/Vaunrable-Oracle-manipulation.sol
+++ AUDITING/Oracle-Manipulation/Vaunrable-Oracle-manipulation.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IPriceFeed {
+    function getPrice() external view returns (uint256);
+}
+
+contract OracleVulnerable {
+    IPriceFeed public priceFeed;
+    mapping(address => uint256) public balances;
+    
+    constructor(address _priceFeed) {
+        priceFeed = IPriceFeed(_priceFeed);
+    }
+    
+    function swapTokens(uint256 amount) public {
+        uint256 currentPrice = priceFeed.getPrice();
+        uint256 tokensToReceive = amount * currentPrice;
+        balances[msg.sender] += tokensToReceive;
+    }
+}
