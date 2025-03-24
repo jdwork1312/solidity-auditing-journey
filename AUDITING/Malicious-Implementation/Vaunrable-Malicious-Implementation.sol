5a5df11707e1149beb3970afa04ef88ce3d5537d
jdwork1312 <jdwork1312@gmail.com>
2025-03-24 17:02:05 +0200

    Update Vaunrable-Malicious-Implementation.sol

--- AUDITING/Malicious-Implementation/Vaunrable-Malicious-Implementation.sol
+++ AUDITING/Malicious-Implementation/Vaunrable-Malicious-Implementation.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IToken {
+    function transfer(address recipient, uint256 amount) external;
+}
+
+contract MaliciousImplementationVulnerable {
+    address public owner;
+    mapping(address => uint256) public balances;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function setToken(address tokenAddress) public {
+        // No validation of token implementation
+        IToken token = IToken(tokenAddress);
+        token.transfer(owner, balances[msg.sender]);
+    }
+}
